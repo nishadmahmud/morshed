ab36cd01c7e18ccf0f15431f639c7e19ea586f0c
Mazharul Islam <mazharulislam3569@gmail.com>
2025-11-19 22:14:28 +0600

    add unoptimize for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -13,6 +14,7 @@ const nextConfig = {
         hostname: "**",
       },
     ],
+    
 
   },
 };
